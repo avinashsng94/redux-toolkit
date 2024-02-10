511cd37be8b6c773f1216c277f668f83b4d197f9
avinashsng <avinash.webintern@gmail.com>
2024-02-10 13:38:49 +0530

    part-6-useSelector files uploaded

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";   
+import ToDoList from "./components/ToDoList";
+
+function App() {
+  return (
+    <div className="App">  
+    {/* To do list */}
+    <ToDoList />
+    </div>
+  );
+} 
+
+export default App;
+
+
